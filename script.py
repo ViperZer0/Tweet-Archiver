9ef57f895d9f894ef49c25073fad5fe1e99bedb3
ViperZer0 <viperzer0thehackingreptile@gmail.com>
2022-09-07 14:31:58 -0700

    Added help option

--- script.py
+++ script.py
@@ -113,42 +113,59 @@ class TweetRetriever():
         return response
 
 class Parser:
-    def __init__(self,args,legend):
+    def __init__(self,args,legend,helpStr):
         self.legend = legend
         self.args = args
+        self.helpString = helpStr
+         
+    def help(self):
+        print(self.helpString)
 
     def hasArg(self, arg):
         return self.legend[arg] in self.args
     
     def getArgPos(self, arg):
         return self.args.index(self.legend[arg]) 
-
+    
 if __name__ == '__main__':
     p = Parser(sys.argv,
             {
              'getUser':'-g',
              'printTweet':'-p',
              'updateTweets':'-u',
-             'forceUpdate':'-U'
-             }
+             'forceUpdate':'-U',
+             'help':'-h'
+             },
+            ("Usage\n\tpython script.py [options]\n\nOptions:\n"
+             "\t-g <username>\tGets <username>'s user id\n"
+             "\t-p\t\tPrints a random tweet from the database\n"
+             "\t-u\t\tUpdates database with tweets posted since last update\n"
+             "\t-U\t\tTries to update as many tweets as possible. Use if the database is missing older tweets.\n"
+             "\t-h\t\tPrints this help menu")
             )
 
     t = TweetRetriever()
 
-    if p.hasArg("getUser"):
+    
+    if p.hasArg("help"):
+        p.help()
+    elif p.hasArg("getUser"):
         #Get next argument
         pos = p.getArgPos("getUser") + 1
         user = t.getUserId(sys.argv[pos])
         print("Display Name: {}\nUsername: {}\nID: {}\n".format(user.data.name,user.data.username,user.data.id),end="")
 
-    if p.hasArg("forceUpdate"):
+    elif p.hasArg("forceUpdate"):
         t.updateDatabase(allTweets=True)
 
-    if p.hasArg("updateTweets") and not p.hasArg("forceUpdate"):
+    elif p.hasArg("updateTweets") and not p.hasArg("forceUpdate"):
         t.updateDatabase(allTweets=False)
 
-    if p.hasArg("printTweet"):
+    elif p.hasArg("printTweet"):
         print(t.getRandomTweet()[1])
+
+    else:
+        p.help()
 
     
     
